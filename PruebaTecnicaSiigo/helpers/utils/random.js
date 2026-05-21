7ef25c1aa2e072a3b620be7e9f1fa3c00c4ad387
Santiago Quijano <saquijano95@gmail.com>
2026-05-21 12:24:19 -0500

    delete prueba

--- PruebaTecnicaSiigo/helpers/utils/random.js
+++
@@ -1,6 +0,0 @@
-function generateRandomId(length = 10) {
-    const min = Math.pow(10, length - 1);
-    const max = Math.pow(10, length) - 1;
-    return Math.floor(Math.random() * (max - min + 1) + min).toString();
-}
-module.exports = {generateRandomId};
